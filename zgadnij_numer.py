25768f64be69d6c7238821ff2f1dd15384aa8213
mike <=>
2020-04-21 11:03:41 +0200

    update to zgadnij numer - ignore

--- zgadnij_numer.py
+++ zgadnij_numer.py
@@ -24,6 +24,7 @@ def guess_number(a, b):
 
 if __name__ == "__main__":
     liczba = randint(0, 1001)
+    print(liczba)
     while game:
         try:
             user_quess = int(input("podaj liczbe: "))
